546a818274ade56081bc50636307ba3432e6f1c8
Gabe Durand <gdurand@jlgarchitects.com>
2023-05-27 22:46:34 -0500

    initial commit

---
+++ addNumbers.php
@@ -0,0 +1,9 @@
+<?php
+$odd_numbers = [1,3,5,7,9];
+$first_odd_number = $odd_numbers[0];
+$second_odd_number = $odd_numbers[1];
+
+echo "The first odd number is $first_odd_number\n";
+echo "The second odd number is $second_odd_number\n";
+
+?>
